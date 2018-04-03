1bef0ce30515081e86654c4e6c1bf50a383f123e
Evan Wilt <ev.wilt@gmail.com>
2018-04-03 13:12:22 -0400

    Added Material Design Lite, started Frequency Analysis Component, added CAS login

---
+++ src/Components/FrequencyAnalysis.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FrequencyAnalysis extends Component {
+  render() {
+    return (
+      <div className="Frequency-Analysis">
+        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+          Button
+        </button>
+      </div>
+    );
+  }
+}
+
+export default FrequencyAnalysis;
