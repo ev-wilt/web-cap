c36b4d5c6ac3222dc60f1952be5f06a991889953
Evan Wilt <ev.wilt@gmail.com>
2018-04-03 16:55:01 -0400

    Started plaintext/ciphertext entry page

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import './Welcome.css'
 
 class Welcome extends Component {
   render() {
     return (
-      <div className="Welcome">
-        {this.props.children}
-        <h2>Welcome!</h2>
+      <div className="Welcome mdl-layout">
+        <div className="mdl-grid">
+          <form>
+            <h4>Plaintext</h4>
+            <textarea rows="20" cols="75" name="plaintext" /><br/>
+            <h4>Ciphertext</h4>
+            <textarea rows="20" cols="75" name="ciphertext" /><br/>
+            <input
+              className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+              type="submit" 
+              value="Submit"
+            />
+          </form>
+        </div>
       </div>
     );
   }
