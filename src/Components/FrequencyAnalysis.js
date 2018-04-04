eb781167cbb7b54a54ed1e5af67e4d6c4a415d05
Jake White <squirrelboy1225@gmail.com>
2018-04-04 01:08:21 -0400

    Implemented sample frequency chart

--- src/Components/FrequencyAnalysis.js
+++ src/Components/FrequencyAnalysis.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import ReactFC from 'react-fusioncharts';
+
+Charts(FusionCharts)
+var chartConfigs = {
+  type: "Column2D",
+  className: "fc-column2d",
+  dataFormat: "JSON",
+  dataSource: {
+      chart:{},
+      data: [{
+        label: "a",
+        value: 3
+      }, {
+        label: "b",
+        value: 2
+      }, {
+        label: "c",
+        value: 4
+      }]
+  }
+}
 
 class FrequencyAnalysis extends Component {
   render() {
     return (
       <div className="Frequency-Analysis">
         {this.props.children}
-        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-          Button
-        </button>
+        <ReactFC {...chartConfigs} />
       </div>
     );
   }
