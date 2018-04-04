553442e51730b85530736aa6c838441eaa8bd437
Evan Wilt <ev.wilt@gmail.com>
2018-04-04 15:46:07 -0400

    Save split ciphertext in cipherArray

--- src/Components/FrequencyAnalysis.js
+++ src/Components/FrequencyAnalysis.js
@@ -17,13 +17,24 @@ let chartConfigs = {
 class FrequencyAnalysis extends Component {
   constructor(props) {
     super (props);
+    this.state = {
+      cipherArray: []
+    };
 
     this.splitCipher = this.splitCipher.bind(this);
   }
 
   splitCipher(splitSize) {
     let ciphertext = this.props.state.ciphertext;
-    
+    ciphertext = ciphertext.trim();
+    this.setState({cipherArray: []});
+
+    while(ciphertext !== "") {
+      this.state.cipherArray.push(ciphertext.slice(0, splitSize + 1));
+      ciphertext = ciphertext.slice(splitSize + 1);
+    }
+
+    console.log(this.state.cipherArray);
   }
 
   render() {
@@ -35,7 +46,7 @@ class FrequencyAnalysis extends Component {
             <div className="mdl-cell mdl-cell--4-col">
               <button
                 className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-                onClick={this.splitCipher(1)}
+                onClick={() => this.splitCipher(0)}
               >
                 Single
               </button>
@@ -43,7 +54,7 @@ class FrequencyAnalysis extends Component {
             <div className="mdl-cell mdl-cell--4-col">
               <button
                 className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-                onClick={this.splitCipher(2)}
+                onClick={() => this.splitCipher(1)}
               >
                 Double
               </button>
@@ -51,7 +62,7 @@ class FrequencyAnalysis extends Component {
             <div className="mdl-cell mdl-cell--4-col">
               <button
                 className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-                onClick={this.splitCipher(3)}
+                onClick={() => this.splitCipher(2)}
               >
                 Triple
               </button>
@@ -59,7 +70,7 @@ class FrequencyAnalysis extends Component {
           </div>
           <textarea
             readOnly={true}
-            value={this.props.state.ciphertext}
+            value={this.state.cipherArray}
             rows="20"
             cols="65"
             id="frequency-output"
