7d3a9ed65868e42b0e7d39b6382c35c8d0e52ccf
Evan Wilt <ev.wilt@gmail.com>
2018-04-09 12:46:54 -0400

    Completed worksheet design

--- src/Components/Worksheet.js
+++ src/Components/Worksheet.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import FusionCharts from 'fusioncharts';
-import Charts from 'fusioncharts/fusioncharts.charts';
-import ReactFC from 'react-fusioncharts';
-
-Charts(FusionCharts);
 
 class Worksheet extends Component {
   constructor(props) {
     super (props);
-    this.state = {};
+    this.state = {
+      alphabet: "a b c d e f g h i j k l m n o p q r s t u v w x y z",
+      guess: "- - - - - - - - - - - - - - - - - - - - - - - - - -",
+      output: ""
+    };
 
+    this.handleGuessChange = this.handleGuessChange.bind(this);
   }
+
+  handleGuessChange(event) {
+    this.setState({guess: event.target.value});
+
+    let guess = event.target.value.split(" ").join("");
+    let alphabet = this.state.alphabet.split(" ").join("");
+    let newOutput = this.props.state.ciphertext;
+
+    for (let i = 0; i < guess.length; ++i) {
+      if (guess[i] !== "-") {
+        newOutput = newOutput.split(alphabet[i]).join(guess[i]);
+      }
+    }
+
+    this.setState({output: newOutput});
+  }
+
 
   render() {
     return (
@@ -19,11 +36,21 @@ class Worksheet extends Component {
           <h4>Worksheet</h4>
           <textarea
             readOnly={true}
-            rows="20"
-            cols="65"
-            id="frequency-output"
+            value={this.state.alphabet}
+            cols="50"
           />
-          <h4>Graphical Analysis</h4>
+          <textarea
+            onChange={this.handleGuessChange}
+            value={this.state.guess}
+            cols="50"
+          />
+          <h4>Output</h4>
+          <textarea
+            readOnly={true}
+            value={this.state.output}
+            rows="20"
+            cols="50"
+          />
         </div>
       </div>
     );
